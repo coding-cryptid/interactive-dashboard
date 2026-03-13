48aeca5e0911c79458e364ced7c1691dcb2629cc
Gianna Cosenza <giacosey28@gmail.com>
2026-03-13 14:24:08 -0400

    Used % in a for loop to check for even or odd

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ function greet(name) {
     return "Hello, " + name + "!"
 }
 
-console.log(greet("Gianna"));
+let person = {
+    name:"Gianna",
+    age: 19,
+    role: "Developer",
+    active: true
+}
+
+console.log(greet(person.name));
+console.log(person);
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i=0; i<numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i] + " is even");
+    } else {
+        console.log(numbers[i] + " is odd");
+    }
+}
